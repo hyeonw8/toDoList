654302507fc1e148e4dea4627c9b258c9b6a275e
hyeonw8 <whgusrud223@gmail.com>
2024-05-17 14:14:17 +0900

    Update: TodoForm 수정
    비제어 컴포넌트 방식으로 변경 적용

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,47 +1,74 @@
-import { useState } from 'react';
+const TodoForm = ({ setTodos }) => {
+  // const [newTitle, setNewTitle] = useState('');
+  // const [newText, setNewText] = useState('');
 
-const TodoForm = ({ todos, setTodos }) => {
-  const [newTitle, setNewTitle] = useState('');
-  const [newText, setNewText] = useState('');
+  // TODO: 함수명은 동사 또는 on(이벤트핸들러한정)로 시작하는 것이 범용적 코딩컨벤션
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (!newTitle.trim() || !newText.trim()) {
+  //     alert('제목과 내용을 모두 입력해 주세요!');
+  //     setNewTitle('');
+  //     setNewText('');
+  //     return;
+  //   }
 
-  const handleSubmit = (e) => {
+  //   const newTodo = {
+  //     id: Date.now(),
+  //     title: newTitle,
+  //     text: newText,
+  //     isDone: false,
+  //   };
+  //  // console.log(newTodo);
+  //   setTodos([...todos, newTodo]);
+  //   setNewTitle('');
+  //   setNewText('');
+  // };
+
+  // 비제어 컴포넌트 방식, input 태그에는 name만 적어주면 됨.
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (newTitle === '' || newText === '') {
-      alert('제목과 내용을 모두 입력해 주세요!');
-      setNewTitle('');
-      setNewText('');
-      return;
+
+    const formData = new FormData(e.target);
+    const title = formData.get('title');
+    const text = formData.get('text');
+
+    if(!title.trim() || !text.trim())
+      return alert('제목과 내용을 입력해주세요!')
+
+    const nextTodo = {
+      id: Date.now(),
+      title,
+      text,
+      isDone: false,
     }
 
-    const newTodo = {
-      id: Date.now(),
-      title: newTitle,
-      text: newText,
-      isDone: false,
-    };
-   // console.log(newTodo);
-    setTodos([...todos, newTodo]);
-    setNewTitle('');
-    setNewText('');
-  };
+    console.log('nextTodo >> ', nextTodo);
+
+    // useState의 비동기성
+    setTodos((prevTodos) => [...prevTodos, nextTodo]);
+    // 맨 앞에 넣어주고 싶으면 [nextTodo, ...prevTodos]
+    e.target.reset();
+  }
 
   return (
-    <form className='form' onSubmit={handleSubmit}>
+    <form className='form' onSubmit={onSubmit}>
       <label htmlFor="inputTitle"> 제목 </label>
       <input
         type="text"
         placeholder="오늘의 할 일을 적어주세요"
-        value={newTitle}
+        // value={newTitle}
+        name='title'
         className="inputTitle"
-        onChange={(e) => setNewTitle(e.target.value)}
+        // onChange={(e) => setNewTitle(e.target.value)}
       />
       <label htmlFor="inputContent"> 내용 </label>
       <input
         type="text"
         placeholder="세부 내용을 적어주세요"
-        value={newText}
+        // value={newText}
+        name='text'
         className="inputContent"
-        onChange={(e) => setNewText(e.target.value)}
+        // onChange={(e) => setNewText(e.target.value)}
       />
       <button className='addButton' type="submit"> 추가하기 </button>
     </form>
